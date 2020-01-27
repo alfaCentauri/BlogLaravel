92dc3cabc3af1742e1933925e6f5684c311f3fdb
Ricardo Presilla <ricardopresilla@gmail.com>
2020-01-27 17:04:10 -0400

    Creadas rutas.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+/**
+ * Expresiones regulares para filtar el parametro id.
+ */
+Route::get('user/{id}', function ($id) {
+    return 'Indice: '.$id;
+})->where('id', '[0-9]+');
+/**
+ * Ruta de prueba.
+ */
+Route::get('/articulo', function () {
+    return 'Hola mundo';
+});
+/**
+ * Prefijo a un grupo de rutas de los articulos. Cada ruta tiene un nombre único.
+*/
+Route::prefix('articles')->group(function () {
+    Route::get('/view', function () {
+        return 'Lista de articulos.';
+    })->name('articlesList');
+    //
+    Route::get('/show/{id}', function ($id) {
+        return 'Detalles del articulo #'.$id;
+    })->name('articleShow');
+    //
+    Route::get('/delete', function () {
+        return 'Borrado de articulos.';
+    })->name('articleDelete');
+});
